802b38c3220f95443c3585a7d69aa70044262b84
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2015-02-25 17:43:07 -0500

    scriptCleanForContacts-advanced functions from David Eads based on ntpepData.xlsx instead of ntpepInfo.xlsx (my fault for pointing to wrong xlsx) but this script's currently structured output (as written to the console) would require me to redesign my JSON prototype (stateInfoList.json), which works well with my JavaScript and displays well on the web page. I structured stateInfoList.json so that firstLast is a first or second level object, then productTypes is a list nested under firstLast.
    I’m hesitant to move forward with this script’s outputted JSON structure wherein productType is an object on a level above first, Last, title, phone, and email and productType (I’m assuming based on what I see in console - the actual processed_data.json file didn’t get written and saved to my project’s directory); the data scientists said such a JSON structure would create a lot of duplicate data (repeated instances of values for first, Last, title, phone, email).
    
    I’m inclined to agree with data scientists’ advice and stick to how my JSON is structured in stateInfoList.json. This means I will struggle to edit this py script so that all the first, Last, title, phone, and email values are associated with their respective productTypes, then delete the dup instances. The py script solution might be to write a few new lines just under line 49 of this current version of scriptCleanForContacts.py, but I'm stumped; I’ve only had 3 python lessons.
    
    If I can’t figure it out by tomorrow (2/26), I’ll resign to copying, cutting, and pasting data from the 1st tab of ntpepData.xlsx into it's 2nd tab (contacts). I’ve already done Alabama through New Jersey.
    
    Then after that, to convert the data on contacts to JSON, I’ll borrow from this scriptCleanForContacts.py to read from the contacts tab of ntpepData.xlsx and write to JSON. Or I could save that tab as CSV and try the converter on http://www.convertcsv.com/csv-to-json.htm.

--- scriptCleanForContacts.py
+++ scriptCleanForContacts.py
@@ -1,235 +1,92 @@
 #!/usr/bin/env python
 
-#import csv
-import xlrd
+import xlrd     # Library that processes excel files
+import json     # Library for processing / writing JSON
 
-# from slugify import slugify # where is slugify defined?
+from slugify import slugify  # Library to slugify strings
+from pprint import pformat   # Pretty print output          # ~ which of these two imports, slugify or pformat, are switching uppercase to lowercase?
 
-# from util import make_headers, clean_data #not needed because:
-#  a) I'm skipping clean_data function
-#  b) instead of importing make_headers, I copied, cut, and pasted from it. source: https://github.com/nprapps/leso/blob/master/util.py 
+# File to be processed
+IMPORT_FILE = 'ntpepInfo.xlsx'
+OUTPUT_FILE = 'processed_data.json' # ~ I changed to 'stateInfoList.json' from 'processed_data.json' then back again
 
-# Ready to quit on this Python script on 2/22/15 at 11:53 AM b/c there is too much I do not understand in this venture to use Python to consolidate product type columns into one column. 
-    # What would I do with all the first, last, title, phone, email headers currently in ntpepInfo.xlsx?
-    # It would take more time to strategize an automated solution and learn a function for getting rid of duplicate individuals than to copy, cut, and paste manually.
-        # If I pick this back up: 
-            # maybe use .zip() below the function def
-            # maybe use .join() inside the function def
-            # Riaz said "explore APIs." Try Google Refine.
+def process_data(): # ~ I'm guessing ntpepInfo.xlsx does not need to be inserted into the brackets
+    """
+    The main processing function.
+    """
 
-#IMPORT_FILE = "ntpepInfo.xlsx" # maybe this line should instead be deleted and a with open statement should go below funtion definition
+    # Open the file
+    workbook = xlrd.open_workbook(IMPORT_FILE)
+
+    # Datemode is required for processing dates in Excel files
+    datemode = workbook.datemode
+
+    worksheet = workbook.sheet_by_name('ntpepInfo')
+
+    # Extract headers from first row of worksheet
+    headers = make_headers(worksheet)
+    print 'Headers are: %s' % pformat(headers) # ~ %s tells Python to interpret input as a string. % tells Python to return remainder.
+
+    # Output will be a list of dictionaries which will be written to JSON
+    output = []
+
+    # Main processing loop, starts with second row
+    row_idx = 1
+
+    # Loop over rows
+    while row_idx < worksheet.nrows:
+        cell_idx = 0
+        row_dict = {}
+        while cell_idx < worksheet.ncols:
+            header = headers[cell_idx]
+            value = worksheet.cell_value(row_idx, cell_idx)
+
+            # Example of processing a specific column
+            if header == 'Polymer_Concrete_Overlays_\(PCO\)':
+                if value == 'No':
+                    value = 0
+                else:
+                    value = 1 # ~ returned value should = the next 5 cells to the right of 'Yes.' (first, Last, title, phone, and email). Is this already done? How do this? # maybe try testing for null instead. if not null, pull in those 5 values from data dict.
+
+            # Example of splitting a column for json 
+            # ~ looking at ntpepInfo.xlsx rather than ntpepData.xlsx (which I erroneously indicated last weekend), I don't want to split any values that are inside the 20 product type columns, 
+            # ~ but maybe instead I want to append the product types to a list, as if they were structured as they are in 2nd tab of ntpepData.xlsx (tab is called contacts)
+            
+            # if header == 'producttypes': # ~ i would need to write this new 'productTypes' list into the dict/json
+            #     value = value.split(',')
+
+            #     row_dict[header] = value
+            #     cell_idx += 1
+
+          # ~ if header == 'email':
+                # ~ if value email is duplicated across the row...delete dups only after cataloguing which product types that individual is associated with.
+
+
+        print 'Processed row: %s' % pformat(row_dict) # ~ %s means return as string
+
+
+    # Write the data to JSON
+    with open(OUTPUT_FILE, 'w') as f:
+        json.dump(output, f)
+
 
 def make_headers(worksheet):
     """Make headers"""
-    dictHeaders = {}
+    headers = {}
     cell_idx = 0
     while cell_idx < worksheet.ncols:
-        
-        if header == "Asphalt_Release_Agents_(ARA)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ") # I replaced "slugify(cell_value)" with "cell_value" b/c Where is slugify defined?
-        
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Concrete_Admixtures_(CADD)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Concrete_Curing_Compounds_(CCC)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Erosion_Control_Products_(ECP)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Geotextiles_and_Reinforced_Geosynthetics_(GTX/REGEO)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "High_Density_Polyethylene_Plastic_Pipe_(HDPE)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Hot-Mix_Asphalt_Crack_Sealers_(HMA_CS)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Pavement_Marking_Materials_(PMM)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Portland_Cement_Concrete_Joint_Sealants_(PCC_JS)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Portable_Changeable_Message_Signs and Flashing_Arrow_Panels_(PCMS-FAP)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Polymer_Concrete_Overlays_(PCO)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Protective_Coatings: Structural_Steel_Coatings and Concrete_Coating_Systems_(SSC-CCS)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Concrete_Coatings_Systems_(CCS)_ONLY":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Polypropylene_Pipe_(PPP)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Polyvinyl_Chloride_Drainage_Pipe_(PVC)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Raised_Pavement_Markers__Snowplowable_Raised_Pavement_Markers_(RPM-SRPM)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Rapid_Set_Concrete_Patch_Materials_(RSCPM)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Reinforcing_Steel and Welded_Wire_Reinforcement_(REBAR-WWR)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Sign_Sheeting_Materials_ and Roll_Up_Signs_(SSM-RUP)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        elif header == "Temporary_Traffic_Control_Devices_(TTCD)":
-            cell_type = worksheet.cell_type(0, cell_idx)
-            cell_value = worksheet.cell_value(0, cell_idx)
-            cell_value = cell_value.replace("_"," ")
-
-            if cell_type == 1:
-                headers[cell_idx] = cell_value
-            cell_idx += 1
-
-        #else: # else is optional
-            #whether here in the else statement or in a separate function, find a way to zip the twenty sets of first, last, title, email, and info into one set.
-
-    # return headers #I'm too tired to figure out how to catch headers into var and put into dict.
-    # print headers
-
-        row_dict[header] = cell_value
+        cell_type = worksheet.cell_type(0, cell_idx)
+        cell_value = worksheet.cell_value(0, cell_idx)
+        cell_value = slugify(cell_value).replace('_',' ') # ~ I changed from ('-', '_'). ( )'s must be reserved characters. I put \ in front of ( and ) in the xlsx but it made no difference
+        if cell_type == 1:
+            headers[cell_idx] = cell_value
         cell_idx += 1
 
-    writer.writerow(row_dict)
-    row_idx += 1
-
     return headers
-    print headers
+    
 
-# with open("ntpepInfo.xlsx", "r") as ntpepInfo_file:
-#     dictHeaders = make_headers(ntpepInfo_file) # ~ do i need to put in 2nd argument?
-#     print "\ndictHeaders is", dictHeaders
-#     print "\n"
+# This allows the script to be run from the command line
+if __name__ == "__main__":
+    process_data()
 
 
-# below: what's left in clean.py that I may use
-   # f = open("src/leso.csv", "w")
-   #  writer = csv.DictWriter(f, fieldnames=headers.values())
-   #  writer.writeheader()
-   #  for worksheet in worksheets:
-   #      sheet = workbook.sheet_by_name(worksheet)
-   #      clean_data(sheet, writer, headers, datemode)
