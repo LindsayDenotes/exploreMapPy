c032e617caaa14c02e733cfe3e38296bc6503f99
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2015-02-28 11:30:20 -0500

    scriptCleanForContacts is reading from ntpepInfo.xlsx, worksheet is ntpepInfo. Headers are in a dict, values are in a dict. Same number of lines in both dicts. To do next: make dicts into lists, zip them into a new list. Name new list keysValuesForJson. Re: my long commit message on 2/25: I'm now willing to wiggle on JSON design.

--- scriptCleanForContacts.py
+++ scriptCleanForContacts.py
@@ -4,13 +4,13 @@ import xlrd     # Library that processes excel files
 import json     # Library for processing / writing JSON
 
 from slugify import slugify  # Library to slugify strings
-from pprint import pformat   # Pretty print output          # ~ which of these two imports, slugify or pformat, are switching uppercase to lowercase?
+from pprint import pformat   # Pretty print output
 
 # File to be processed
 IMPORT_FILE = 'ntpepInfo.xlsx'
-OUTPUT_FILE = 'processed_data.json' # ~ I changed to 'stateInfoList.json' from 'processed_data.json' then back again
+OUTPUT_FILE = 'processed_data.json'
 
-def process_data(): # ~ I'm guessing ntpepInfo.xlsx does not need to be inserted into the brackets
+def process_data():
     """
     The main processing function.
     """
@@ -25,45 +25,46 @@ def process_data(): # ~ I'm guessing ntpepInfo.xlsx does not need to be inserted
 
     # Extract headers from first row of worksheet
     headers = make_headers(worksheet)
-    print 'Headers are: %s' % pformat(headers) # ~ %s tells Python to interpret input as a string. % tells Python to return remainder.
+    print '\nHeaders are: %s' % pformat(headers)
+    
+    # Exract values from the rest of rows of worksheet
+    values = make_values(worksheet)
+    print '\nValues are: %s' % pformat(values)
 
-    # Output will be a list of dictionaries which will be written to JSON
+
+
+
+    # Output will be a list of dictionaries which will be written to JSON. ~Does this apply only to the make_headers function? I'd like it to include make_values function, too.
     output = []
 
+    
     # Main processing loop, starts with second row
-    row_idx = 1
+    row_idx = 1 # ~ 1 means there is a value there, right? doesn't mean second row? unaltered code from Eads didn't include any function for what to do with rows 1 through 52, right?
 
     # Loop over rows
     while row_idx < worksheet.nrows:
-        cell_idx = 0
+        cell_idx = 0 # ~ this is the header cell, agency, or is it the first row down value, Alabama Department of Transportation? 
         row_dict = {}
         while cell_idx < worksheet.ncols:
             header = headers[cell_idx]
-            value = worksheet.cell_value(row_idx, cell_idx)
+            value = worksheet.cell_value(row_idx, cell_idx) # ~ what does this line do? 2/28 this line 46 is first time 'cell_value' is mentioned
 
             # Example of processing a specific column
-            if header == 'Polymer_Concrete_Overlays_\(PCO\)':
+            if header == 'polymer_concrete_overlays_pco': # crf: store each productType header such as this one into a var
                 if value == 'No':
                     value = 0
                 else:
-                    value = 1 # ~ returned value should = the next 5 cells to the right of 'Yes.' (first, Last, title, phone, and email). Is this already done? How do this? # maybe try testing for null instead. if not null, pull in those 5 values from data dict.
+                    value = 1
 
-            # Example of splitting a column for json 
-            # ~ looking at ntpepInfo.xlsx rather than ntpepData.xlsx (which I erroneously indicated last weekend), I don't want to split any values that are inside the 20 product type columns, 
-            # ~ but maybe instead I want to append the product types to a list, as if they were structured as they are in 2nd tab of ntpepData.xlsx (tab is called contacts)
-            
-            # if header == 'producttypes': # ~ i would need to write this new 'productTypes' list into the dict/json
+
+            # Example of splitting a column for json
+            # if header == 'producttypes':
             #     value = value.split(',')
 
-            #     row_dict[header] = value
-            #     cell_idx += 1
+            row_dict[header] = value
+            cell_idx += 1
 
-          # ~ if header == 'email':
-                # ~ if value email is duplicated across the row...delete dups only after cataloguing which product types that individual is associated with.
-
-
-        print 'Processed row: %s' % pformat(row_dict) # ~ %s means return as string
-
+        #print 'Processed row: %s' % pformat(row_dict)
 
     # Write the data to JSON
     with open(OUTPUT_FILE, 'w') as f:
@@ -77,13 +78,28 @@ def make_headers(worksheet):
     while cell_idx < worksheet.ncols:
         cell_type = worksheet.cell_type(0, cell_idx)
         cell_value = worksheet.cell_value(0, cell_idx)
-        cell_value = slugify(cell_value).replace('_',' ') # ~ I changed from ('-', '_'). ( )'s must be reserved characters. I put \ in front of ( and ) in the xlsx but it made no difference
+        cell_value = slugify(cell_value).replace('-', '_')
         if cell_type == 1:
             headers[cell_idx] = cell_value
         cell_idx += 1
 
     return headers
-    
+
+
+def make_values(worksheet):
+    """Make values"""
+    values = {}
+    cell_idx = 1 # ~ 1 refers to the 2nd column; 1 = asphalt_release_agents_ara
+    while cell_idx < worksheet.ncols:
+        cell_type = worksheet.cell_type(1, cell_idx)
+        cell_value = worksheet.cell_value(1, cell_idx)
+        #cell_value = slugify(cell_value).replace('-', '_')
+        if cell_type == 1:
+            values[cell_idx] = cell_value
+        cell_idx += 1
+
+    return values
+
 
 # This allows the script to be run from the command line
 if __name__ == "__main__":
