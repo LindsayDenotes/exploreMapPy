fe6f944f749fb4f7d8be50d95613c4bd639e7c1d
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2015-04-11 19:52:47 -0400

    scriptCleanForContacts.py - now writes to correct JSON structure.

--- scriptCleanForContacts.py
+++ scriptCleanForContacts.py
@@ -4,11 +4,11 @@ import xlrd     # Library that processes excel files
 import json     # Library for processing / writing JSON
 
 from slugify import slugify  # Library to slugify strings
-from pprint import pformat   # Pretty print output
+from pprint import pformat, pprint   # Pretty print output
 
 # File to be processed
 IMPORT_FILE = 'ntpepInfo.xls' # not .xlsx
-OUTPUT_FILE = 'processed_data.json'
+OUTPUT_FILE = 'stateInfoList.json'
 
 def process_data():
     """
@@ -43,194 +43,55 @@ def process_data():
                 colValuesList = data[keys[idx]]
                 colValuesList.append(val.value)
             # print "colValuesList idx:{0} len:{1} {2}".format(idx,len(colValuesList),colValuesList) # def don't delete this line and block above, even if you comment out
+            # Note: I had to put spaces in cells without values and replace .strip with .title in order to have empty strings returned
     
-        # Why aren't column indices in their proper order? Liz said proly something goofy in my data. Note: I had to put spaces in cells without values.
-        # [0] abb, [1] Last, [2] title, [3] product, [4] agency, [5] firstLast, [6] phone (correct), [7] email (correct), [8] first
-        firstLastList = list( data.values()[5] ) # list not returning the 5 empty strings and that's okay because the other 3 lists won't return them either so these 4 can be zipped
-        # print "\nfirstLastList", len(firstLastList) # len 274 without set. len137 with set
-        titleList = list( data.values()[2] )
-        # print "\ntitleList", len(titleList) #len274 without set. len127 set, should be 137, there must be dup titles.
-        phoneList = list( data.values()[6] )
-        # print "\nphoneList", len(phoneList) #len274 without set. len135 set, should be 137, there must be dup phones. 
-        emailList = list( data.values()[7] )
-        # print "\nemailList", len(emailList) #len274 without set. len134 set, should be 137, there must be dup emails. 
-        
-        abbsList = list( data.values()[0] )
-        # print "\nabbsList",len(abbsList) #len284 without set. len51 set.
-        agencyList = list( data.values()[4] ) 
-        # print "\nagencyList", len(agencyList) #len284 without set. len51 set.
-        
-    # Create dict from abbsList where item becomes key, separate empty dicts become values. I used a dictionary comprehension instead of .dict()
-        # abbsDict = {k: {} for k in abbsList[0:51]}  # Goal 1 completed. 51 empty dicts being created by this line
-        # print "\nabbsDict",abbsDict
-    # Goal 2: insert agency header and agency values into the 51 empty dicts
-        # problem with the loop below is agency doesn't match with main key. Fixed beneath this while loop by zipping the abb and agency lists together, then removing dups
-        # idx = 0
-        # for state in abbsDict: # this gives keys
-        #     abbsDict[state] = {'agency': agencyList[idx]}
-        #     idx += 1
-        # print "\nabbsDict", abbsDict
-              
-    # Outside while loop - zipping and then setting of lists made inside while loop, creating dicts
-    # in first attempt loop above to create a dict, the agency was mismatched to main state abb key.  
-    abAg = zip(data.values()[0], data.values()[4]) # this zip associates abb col with agency col; creates proper pairing
-    # print "abAg", len(abAg)  #len 284
-    abAgSets = set(abAg) # removed dups
-    # print "abAgSets", len(abAgSets) #len 51   
-    
-    # Below is loop that creates dict with agency matched to main state abb key. possibly later, I'll add contactslist into this dictionary block
-    stateDict = {}
-    for abAgPair in abAgSets: # like saying, to create a key val pair from the zipped and set list of abbs and agencies...
-        stateDict[abAgPair[0]] = {"agency": abAgPair[1]} # position 0 of this zipped list is abb, position 1 is agency. I removed "contactsList": contactsList
-    # print "\nstateDict",stateDict      
-        
-    fLtpeList = zip(data.values()[5], data.values()[2], data.values()[6], data.values()[7]) # skips the 5 empty string states
-    # print "fLtpeList", len(fLtpeList) #len284
-    agList = (data.values()[4])
-    # print "agList", agList #len284. 
-    agFltpeList = zip(agList, fLtpeList) # this zip associates ag with 4 cols that will be the vals in the sub dicts, firstLastDict. List of tuples.
-    # print "agFltpeList", agFltpeList # len 284, was 274 before I replaced .strip with .title in line 40
-    agFltpeSets = set(agFltpeList) # removed dups but screwed up the order!!
-    # print "agFltpeSets", agFltpeSets # len 142, was 137 before I replaced .strip with .title in line 40. 
-    
-    # Below is the loop that creates firstLastDicts that will be wrapped by contactsList
-    firstLastDict = {} 
-    for agFltpeGroup in agFltpeSets: # like saying, to create a key val pair from the zipped and set list of ag and 4 cols...
-        firstLastDict[agFltpeGroup[0]] = {"firstLast": agFltpeGroup[1]} #"title": agFltpeGroup[2], "phone": agFltpeGroup[3], "email": agFltpeGroup[4]}
-    print "\nfirstLastDict", firstLastDict
-    # print "\nfirstLastDict.values()", firstLastDict.values() #len137
-    # contactsList = firstLastDict.values() # maybe don't rename the list
-    # print "contactsList", len(contactsList) #len137
-
-        # for contact in agencyList:
-    #     = firstLastDict.values()
-    
-    data = stateDict
-    return data
-            
-    # {x: x**2 for x in (2, 4, 6)} # This line and what it returns on next line is food for thought
-    # {2: 4, 4: 16, 6: 36}
-
-
-
-    # failed block below. transform into a dictionary with the function dict(). but this only returned first letter of each abb.
-    # abbs_dict = {}
-    # abbs_dict = dict(data.values()[0])
-    # print "abbs_dict", abbs_dict
-
-    # failed block below.
-    # print "abbs_dict",abbs_dict
-    # for key,val in abbsList:
-    #     item = key
-    #     single_col_dict.update(item)
-    # print "single_col_dict",single_col_dict
-
-    # this block below may work for creating firstLast, title, phone, email, productTypes dicts, which may entail .zip( )
-    # num_rows = worksheet.nrows - 1
-    # curr_row = -1
-    # while curr_row < num_rows:
-    #     curr_row += 1
-    #     row = worksheet.row(curr_row)
-    #     rowValuesList = []
-    #     rowValuesList.append(row)
-    #     # print "\nrowValuesList", rowValuesList
-
-        # single_col_dict = {} # create dict
-        # # print "\nzip(keys,rowValuesList",zip(keys,rowValuesList)
-        # for key, value in zip(keys,rowValuesList): #looping through zip list, returns keys and values as strings
-        #     print "key is {0} and value[0] is {1}".format(key, value[0]) # prints key is abb and value is AL
-            
-        # abbVals = []
-        # for abb in key,value[0]:
-        #     value[0] = abb
-        # # print "abb",abb
-        # abbVals.append(abb)
-        # print "abbVals",abbVals
-                # single_col_dict.update(abb)
-            # print "single_col_dict",single_col_dict
-        
-
-            # One abandoned attempt to narrow dict to include just rowValuesList[0], aka, 'abb' values
-            # if key == 'abb':
-            #     single_line_dict[value] = key #returns properties (key, value pairs, i.e., "labels" and "details") as dict
-            # print "single_line_dict",single_line_dict
-
-            
+    # Why aren't column indices in the order they appear in the Excel worksheet? Liz said proly something goofy in my data. 
+    # [0] abb (correct), [1] Last, [2] title, [3] productType, [4] agency, [5] firstLast, [6] phone (correct), [7] email (correct), [8] first
+    abbsList = data.values()[0] 
+    # print "\nabbsList",len(abbsList) #len284 without set. len51 set.
+    agencyList = data.values()[4]
+    # print "\nagencyList", len(agencyList) #len284 without set. len51 set.
+    firstLastList = data.values()[5]  # list not returning the 5 empty strings and that's okay because the other 3 lists won't return them either so these 4 can be zipped
+    # print "\nfirstLastList", len(firstLastList) # len 274 without set. len137 with set
+    titleList = data.values()[2] 
+    # print "\ntitleList", len(titleList) #len274 without set. len127 set, should be 137, there must be dup titles.
+    phoneList = data.values()[6] 
+    # print "\nphoneList", len(phoneList) #len274 without set. len135 set, should be 137, there must be dup phones. 
+    emailList = data.values()[7] 
+    # print "\nemailList", len(emailList) #len274 without set. len134 set, should be 137, there must be dup emails. 
+    productTypes = data.values()[3]
 
         
-    # return data
+    allHeaders = zip(abbsList,agencyList,firstLastList,titleList,phoneList,emailList,productTypes)
+    # print allHeaders
+    allHeaders = set(allHeaders)
 
+    dotDict = {}
 
+    for row in allHeaders:
+        abb,agency,firstLast,title,phone,email,productsString = row
+        if abb not in dotDict:
+            dotDict[abb] = {"agency": agency, "contacts":[]}
+        contact = {"firstLast": firstLast, "title": title, "phone": phone, "email": email}
+        products = productsString.split(",")
+        contact["productTypes"] = products
+        dotDict[abb]["contacts"].append(contact)
 
-                # print "idx,val.value", idx,val.value
-
-                # abbsLists = []
-                # if keys[idx] =='abb': # if condition
-                #     abbsLists.append(val.value)
-                # print "abbsLists",abbsLists
-                # # uniqueAbbLists = list(set(abbsLists))
-                # # print "uniqueAbbLists", uniqueAbbLists
-                # # if val not in abbs: # The in operator can be used to check if an item is present in the 
-                
-                # single_line_dict = {} # create 
-                # print "zip(keys,valValuesList",zip(keys,valValuesList) #keys not matching with vals # zip method returns as list
-            
-                # attempt to delete dups from dict that has dups
-                # result = {}
-                # for key,value in data():
-                #     if value not in result.values():
-                #         result[key] = value
-                # print "value", value
-                # print "result",result
-                # return result
-
-
-
-    # print "data",data
-    # return data    
+    # print dotDict
+    pprint(dotDict)
+    return dotDict
  
-data = process_data() 
+# data = process_data() 
 # output = []
 # output = output.append(data)
-
-# Write the data to JSON
-with open(OUTPUT_FILE, 'w') as f: #~do I need to rename OUTPUT_FILE to processed_data?
-    json.dump(data, f) # replaced output 
-
-
-# def make_headers(worksheet):
-#     """Make headers"""
-#     headers = {}
-#     cell_idx = 0
-#     while cell_idx < worksheet.ncols:
-#         cell_type = worksheet.cell_type(0, cell_idx) # ~ cell type 0 is cell with no value, cell type 1 is cell with value
-#         cell_value = worksheet.cell_value(0, cell_idx)
-#         cell_value = slugify(cell_value).replace('-', '_')
-#         if cell_type == 1:
-#             headers[cell_idx] = cell_value
-#         cell_idx += 1
-
-#     return headers
-
-
-# def make_values(worksheet): # consider adding 2nd arg, num_rows as iter var here and cell_val line below
-#     """Make values"""
-#     values = {}
-#     cell_idx = 0 # ~ 1 refers to the 2nd column; 
-#     while cell_idx < worksheet.ncols:
-#         cell_type = worksheet.cell_type(0, cell_idx) # ~ cell type 0 is cell with no value, cell type 1 is cell with value
-#         cell_value = worksheet.cell_value(1, cell_idx) # ~ don't hardcode a number into first argument, make a var such as row_num or key
-#         #cell_value = slugify(cell_value).replace('-', '_')
-#         if cell_type == 1:
-#             values[cell_idx] = cell_value
-#         cell_idx += 1
-
-#     return values
 
 
 # This allows the script to be run from the command line
 if __name__ == "__main__":
-    process_data()
-    
+    data = process_data()
+    # Write the data to JSON
+    with open(OUTPUT_FILE, 'w') as f: #~do I need to rename OUTPUT_FILE to processed_data?
+        json.dump(data, f) # replaced output    
+
 
 
