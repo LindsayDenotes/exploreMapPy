30912534f4635d1de29adcc71efc3cb077949b78
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2015-04-15 23:26:52 -0400

    exploreMap.js - fixed brackets but now object,object.

--- exploreMap.js
+++ exploreMap.js
@@ -13,76 +13,90 @@ $( document ).ready( function(){
           console.log( "so var selectedState is " + selectedState );
 
           $.each ( data, function( key, val ){
-            console.log ( key, val );//key is "nh" or "fl", val is whats inside json's { }s
+                console.log ( key, val );//key is "nh" or "fl", val is whats inside json's { }s
 
-            var jsonKey = ( key );
-            console.log ( "this is a state key: " + jsonKey );
+                var jsonKey = ( key );
+                console.log ( "this is a state key: " + jsonKey );
 
-            if ( jsonKey == selectedState ){
-              console.log( "selectedState ID " + selectedState + " MATCHES key: " + jsonKey + ". Return that val." );
+                if ( jsonKey == selectedState ){
+                      console.log( "selectedState ID " + selectedState + " MATCHES key: " + jsonKey + ". Return that val." );
 
-              var contacts = [];
-              contacts = ( val.contacts );
-              console.log ( "Referenced by val.contacts, contacts are " + contacts );
+                      var contacts = [];
+                      contacts = ( val.contacts );
+                      console.log ( "Referenced by val.contacts, contacts are " + contacts );
 
-              var theText = "<dl class ='agency " + key + "'>" + val.agency + "</dl>"; // 2/1/15  changed from state to agency";
-              contacts.forEach( function( obj ){
-
-
-                var productTypes = [];
-                productTypes = ( obj.productTypes );
-                console.log( productTypes );
-
-                var productTypesText = " ";//how do I say, if empty string, return nothing? I don't want triple quotes.
-                productTypesText += productTypes;
-                productTypesText = productTypesText.replace(/,/g , "<br/>" );//Why do I have a triple quote in my HTML? g stands for global, replace all matches, and not just the first one. makes it a regular expression(?)
-                console.log(productTypesText);
+                      var theText = "<dl class ='agency " + key + "'>" + val.agency + "</dl>";
 
 
-                var firstLast = ( obj.firstLast );
-                console.log( firstLast );
+                          if ( jsonKey == co,hi,nm,sd,wy ){
+                                console.log( jsonKey + "is one of those 5 empty states");
 
-                var title = ( obj.title );
-                console.log ( title );
+                                contacts.forEach( function( obj ){
 
-                var phone = ( obj.phone );
-                console.log ( phone );
+                                    var theMessage = (obj);
+                                    console.log( theMessage )
 
-                var email = ( obj.email );
-                console.log ( email );
+                                    theText += "<dt class='contacts'>" + contacts + "</dt>"
+                                });
+                          }
 
-//              if firstLast not in contacts:
-                if(firstLast.hasOwnProperty('obj.firstLast')) { ... } // will run
-//              if(firstLast.hasOwnProperty('toString')) { ... } // will not run
+                          else {
+                                contacts.forEach( function( obj ){
 
-                theText += "<dt class='contacts'>" + firstLast + ", " + title + ", " + phone + ", " + email + "</dt>";
-                theText += "<dd class='productTypes'>" + productTypesText + "</dd>";//make this a </br> to prevent extra space?
+                                    var productTypes = [];
+                                    productTypes = ( obj.productTypes );
+                                    console.log( productTypes );//
 
-//             $( "#txtDOT" ).append( "<p id='productTypes'>" + productTypes + "</p>" );
-//             $( "#txtDOT" ).append( "<p id ='firstLast'>" + firstLast + "</p>" );
-
-              });
-
-              $( "#txtDOT" ).html(theText);
+                                    var productTypesText = " ";//how do I say, if empty string, return nothing? I don't want triple quotes.
+                                    productTypesText += productTypes;
+                                    productTypesText = productTypesText.replace(/,/g , "<br/>" );//Why do I have a triple quote in my HTML? g stands for global, replace all matches, and not just the first one. makes it a regular expression(?)
+                                    console.log(productTypesText);//undefined for 5 states
 
 
-               }
+                                    var firstLast = ( obj.firstLast );
+                                    console.log( firstLast );//undefined for 5 states
 
-               else {
-//               console.log( "selectedState ID " + selectedState + " does NOT match key: " + jsonKey + " , so don't return that val." )
-               }
+                                    var title = ( obj.title );
+                                    console.log ( title );//undefined for 5 states
 
-                });
+                                    var phone = ( obj.phone );
+                                    console.log ( phone );//undefined for 5 states
 
-                /*jQuery Selector $() function w optional 2nd parameter to do a search within an event handler
-                $("g").on("click", function (e) {//Using e is just a short for event. You can pass any variable name you desire.
-                     var $e = $(e.target);//target is #something
-                     clicked.css("background", "red");
-                });
-                */
+                                    var email = ( obj.email );
+                                    console.log ( email );//undefined for 5 states
+
+                                    theText += "<dt class='contacts'>" + firstLast + ", " + title + ", " + phone + ", " + email + "</dt>";
+                                    theText += "<dd class='productTypes'>" + productTypesText + "</dd>";//make this a </br> to prevent extra space?
+
+                                });
+                          }
+                $( "#txtDOT" ).html(theText);
+                }
+
+                else {
+                //console.log( "selectedState ID " + selectedState + " does NOT match key: " + jsonKey + " , so don't return that val." )
+                }
+
+          });
+
         });
 
     });
 
 });
+
+                /*
+                  if firstLast not in contacts:
+                          if(firstLast.hasOwnProperty('obj.firstLast')) { ... } // will run
+                          if(firstLast.hasOwnProperty('toString')) { ... } // will not run
+
+                $( "#txtDOT" ).append( "<p id='productTypes'>" + productTypes + "</p>" );
+                     $( "#txtDOT" ).append( "<p id ='firstLast'>" + firstLast + "</p>" );
+
+                jQuery Selector $() function w optional 2nd parameter to do a search within an event handler
+                $("g").on("click", function (e) {//Using e is just a short for event. You can pass any variable name you desire.
+                     var $e = $(e.target);//target is #something
+                     clicked.css("background", "red");
+                });
+                */
 
