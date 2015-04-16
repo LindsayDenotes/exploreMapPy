03dcda14cca5095dacb42c59928891f6c32f90c9
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2015-04-15 21:29:08 -0400

    exploreMap.js - started but stopping an attempt to programatically remove triple quotes appearing on HTML's dt.contacts and dd.productTypes (2 classes). The triple quotes aren't currently displaying but instead undefined objects for each firstLast, title, phone, email, and productTypes objects. I was making better progress by adjusting the styling on the dt.contacts and dd.productTypes, but how do I make an If statement there? I think I'll try doing so in the JS and using attributes like I did to style the clicked on state shape.

--- exploreMap.js
+++ exploreMap.js
@@ -7,7 +7,7 @@ $( document ).ready( function(){
         $( "g" ).on( "click", function (e) {
           console.log( "user clicked " + this.id );
 
-          $( this ).attr( "class", "clicked" ).siblings( "g" ).removeAttr( "class","clicked" );
+          $( this ).attr( "class", "clicked" ).siblings( "g" ).removeAttr( "class","clicked" ); //styling the selected svg shape
 
           selectedState = ( this.id );
           console.log( "so var selectedState is " + selectedState );
@@ -25,17 +25,19 @@ $( document ).ready( function(){
               contacts = ( val.contacts );
               console.log ( "Referenced by val.contacts, contacts are " + contacts );
 
-              var theText = "<p id='agency " + key + "'>" + val.agency + "</p>"; //2/1/15  changed from state to agency";
+              var theText = "<dl class ='agency " + key + "'>" + val.agency + "</dl>"; // 2/1/15  changed from state to agency";
               contacts.forEach( function( obj ){
+
 
                 var productTypes = [];
                 productTypes = ( obj.productTypes );
                 console.log( productTypes );
 
-                var productTypesText = "";
+                var productTypesText = " ";//how do I say, if empty string, return nothing? I don't want triple quotes.
                 productTypesText += productTypes;
-                productTypesText = productTypesText.replace(/,/g , "<br/>" );//g stands for global, replace all matches, and not just the first one. makes it a regular expression(?)
+                productTypesText = productTypesText.replace(/,/g , "<br/>" );//Why do I have a triple quote in my HTML? g stands for global, replace all matches, and not just the first one. makes it a regular expression(?)
                 console.log(productTypesText);
+
 
                 var firstLast = ( obj.firstLast );
                 console.log( firstLast );
@@ -49,8 +51,12 @@ $( document ).ready( function(){
                 var email = ( obj.email );
                 console.log ( email );
 
-                theText += "<p class='contacts'>" + firstLast + ", " + title + ", " + phone + ", " + email + "</br>";
-                theText += "<p class='productTypes'>" + productTypesText + "</p>";//make this a </br> to prevent extra space? Can we switch font to Arial?
+//              if firstLast not in contacts:
+                if(firstLast.hasOwnProperty('obj.firstLast')) { ... } // will run
+//              if(firstLast.hasOwnProperty('toString')) { ... } // will not run
+
+                theText += "<dt class='contacts'>" + firstLast + ", " + title + ", " + phone + ", " + email + "</dt>";
+                theText += "<dd class='productTypes'>" + productTypesText + "</dd>";//make this a </br> to prevent extra space?
 
 //             $( "#txtDOT" ).append( "<p id='productTypes'>" + productTypes + "</p>" );
 //             $( "#txtDOT" ).append( "<p id ='firstLast'>" + firstLast + "</p>" );
@@ -63,14 +69,14 @@ $( document ).ready( function(){
                }
 
                else {
-               console.log( "selectedState ID " + selectedState + " does NOT match key: " + jsonKey + " , so don't return that val." )
+//               console.log( "selectedState ID " + selectedState + " does NOT match key: " + jsonKey + " , so don't return that val." )
                }
 
                 });
 
                 /*jQuery Selector $() function w optional 2nd parameter to do a search within an event handler
                 $("g").on("click", function (e) {//Using e is just a short for event. You can pass any variable name you desire.
-                     var $e = $(e.target);//target is #txtDOT
+                     var $e = $(e.target);//target is #something
                      clicked.css("background", "red");
                 });
                 */
