29c53da74ea9c6457359af6ea5d11e8bdc2936e3
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2015-04-28 22:16:37 -0400

    productMenu.js - removed faulty styling from .clicked class.

--- productMenu.js
+++ productMenu.js
@@ -5,11 +5,17 @@ $( document ).ready( function(){
       console.log ( data );//whole JSON object
 
         $('#productOptions').change(function() {
-            var val = $("#productOptions option:selected").text();
-            alert(val);
+            var selected = $("#productOptions option:selected").text();
+            console.log( "user selected " + selected );
+
+//            $( this ).attr( "class", "selectedProduct" ).siblings( "g" ).removeAttr( "class","selectedProduct" ); //styling the selected svg shapes
+
         });
-
-
     });
-
 });
+
+//alert(selected);
+
+//console.log( "user selected " + this.id );//returned productOptions
+//console.log( "user selected " + this.val ); //returned undefined
+//console.log( "user selected " + this ); //returned [object HTMLSelectElement]
