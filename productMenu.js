d104a1737052c19faa1967505f1d39ca5c0e3720
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2015-04-30 22:11:43 -0400

    productMenu.js - drilled down to elements of productTypes array. Used forEach(function(entry) to return string per element, then used indexOf to check if selected option from dropdown exists in that array. Next, return me that main state key.

--- productMenu.js
+++ productMenu.js
@@ -5,17 +5,58 @@ $( document ).ready( function(){
       console.log ( data );//whole JSON object
 
         $('#productOptions').change(function() {
-            var selected = $("#productOptions option:selected").text();
-            console.log( "user selected " + selected );
+            var selected = $("#productOptions option:selected").text();  //$spans.eq( 3 ).text( jQuery.inArray( "Pete", arr, 2 ) );
+            console.log( "user selected " + selected );//returns not strings on the console
 
-//            $( this ).attr( "class", "selectedProduct" ).siblings( "g" ).removeAttr( "class","selectedProduct" ); //styling the selected svg shapes
+            var selectedObj = selected;
+            console.log ( selectedObj );//Uncaught SyntaxError: Unexpected token A in exploreMap.html line 1
+
+
+//          $( this ).attr( "class", "selectedProduct" ).siblings( "g" ).removeAttr( "class","selectedProduct" ); //styling the selected svg shapes
+
+            $.each ( data, function( key, val ){
+                console.log ( key, val );//key is "nh" or "fl", val is whats inside json's { }s
+
+                var jsonKey = ( key );
+                console.log ( "this is a state key: " + jsonKey );
+
+                var contacts = [];
+                contacts = ( val.contacts );
+                console.log ( "Referenced by val.contacts, contacts are " + contacts );//returns contacts are [object object]. So crack into those with a forEach loop
+
+
+                contacts.forEach( function( obj ){// The forEach() method executes a provided function once per array element.
+
+                    var productTypes = [];
+                    productTypes = ( obj.productTypes );
+                    console.log( productTypes );//an array of strings. How to iterate over each item and make object out of match? BTW, for Array objects, the toString method joins the array and returns one string containing each array element separated by commas.
+                    console.log( typeof productTypes );//object
+
+                    if( Object.prototype.toString.call( productTypes ) === '[object Array]' ) {
+                        console.log ( 'Array!' );
+                    }
+
+                    productTypes.forEach( function( entry ) {
+
+                        console.log( entry );//the element from array
+                        console.log ( typeof entry );//string
+
+                        console.log ( selected );//option the user selected
+                        console.log ( typeof selected );//string
+
+                        if(selected.indexOf( entry )==-1)//returns either the index/number of the start point for the string or a -1 meaning it isn’t there.
+                            console.log("element doesn't exist");
+                        else
+                            console.log("element found");
+
+                    });
+
+
+
+                });
+
+            });
 
         });
     });
 });
-
-//alert(selected);
-
-//console.log( "user selected " + this.id );//returned productOptions
-//console.log( "user selected " + this.val ); //returned undefined
-//console.log( "user selected " + this ); //returned [object HTMLSelectElement]
