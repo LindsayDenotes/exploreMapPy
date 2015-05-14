8987473d0530813e84c6251a2cc19f1a0018e6ef
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2015-05-13 23:19:12 -0400

    exploreMap.js/productMenu.js - I combined this project's two separate JS files into one. The code from productMenu.js is now contained in exploreMap.js and begins on line 82. I need to figure out how to make this web app work so that when the user clicks on a state, the selectedClass doesn't go away; i.e., those white states remain white until the user chooses a new product type from the drop down menu.

--- exploreMap.js
+++ exploreMap.js
@@ -78,6 +78,84 @@ $( document ).ready( function(){
 
         });
 
+
+        $( "#productOptions" ).change( function( e ) {//.on can only bind to one function and .on is used on clicked g already. use .bind to bind ~?an el~? to multiple functions. Can .change do multiple events? it better or replace it.
+
+                    $('.selectedClass').attr('class','');
+
+                    var selected = $("#productOptions option:selected").text();  //$spans.eq( 3 ).text( jQuery.inArray( "Pete", arr, 2 ) );
+        //            console.log( "user selected " + selected );
+        //            console.log( typeof selected );//string
+
+                    $.each ( data, function( key, val ){
+                        console.log ( key, val.contacts );//key is "nh" or "fl", val is whats inside json's { }s
+
+        //                var jsonKey = ( key );
+        //                console.log ( "this is a state key: " + jsonKey );
+
+                        var contacts = [];
+                        contacts = ( val.contacts );
+        //                console.log ( "Referenced by val.contacts, contacts are " + contacts );//returns contacts are [object object]. So crack into those with a forEach loop
+
+
+                        contacts.forEach( function( obj ){// The forEach() method executes a provided function once per array element.
+
+                                var productTypes = [];
+                                productTypes = ( obj.productTypes );
+        //                      console.log( productTypes );//an array of strings. How to iterate over each item and make object out of match? BTW, for Array objects, the toString method joins the array and returns one string containing each array element separated by commas.
+        //                      console.log( typeof productTypes );//object
+
+                                if( Object.prototype.toString.call( productTypes ) === "[object Array]" ) {//test to make sure it is a true array, not array-like object. a prototype function.
+        //                          console.log ( "Array" );
+                                }
+
+
+                                productTypes.every( function( entry ) { //every returns same as forEach from what I've tested so far. The every method executes the provided callback function once for each element present in the array until it finds one where callback returns a falsy value (a value that becomes false when converted to a Boolean). If such an element is found, the every method immediately returns false.
+        //                          console.log( entry );//entry is the element(s) from array
+        //                          console.log ( typeof entry );//string
+        //                          console.log ( selected );//my var, option the user selected
+        //                          console.log ( typeof selected );//string
+
+                                    if (selected.indexOf( entry ) == -1 ) {//returns either the index/number of the start point for the string or a -1 meaning it isn’t there.
+        //                              console.log( "element doesn't exist" );
+                                    }
+
+                                    else {
+        //                              console.log( "element found" );
+
+                                    var productKey = ( key );
+                                        console.log ( "productKey " + productKey );//presence of a productKey means that corresponding state shapes (#g's) should get styled.
+
+                                    var $productStates =  $( "g" )
+
+                                          .filter( function( index ) {
+                                            if ( $( this ).attr( "id" ) == productKey ){//Within the filter function, this refers to each DOM element in turn.
+                                                console.log ( this.id );//GOOD. returned or mn etc, ie, the states that have that product type user selected in the dropdown
+                                                return ( this.id );//running slowly. I think each object has own function. constructor method of object creation inefficient.
+                                            }
+                                          })
+                                            console.log ($productStates);
+
+        //                                        $productStates.css( "fill-opacity", "0.1" );//SUCCESS, but how to remove styling from unselected g's?
+        //                                        $productStates = $productStates.attr("class","selectedClass");//class appears on tag but not actually styled with opacity change
+        //                                        $productStates = $productStates.attr("class","selectedClass").siblings( "g" ).removeAttr( "class","selectedClass" );//class appears but quickly removes itself
+                                                $productStates.attr("class","selectedClass");//.siblings( "g" ).removeAttr( "class","selectedClass" );//class appears on tag but not actually styled with opacity change
+                                                //for line above, I indented one more tab in from where it was and now selectedClass is again appearing on tags but not actually styling the g's.
+//                                                $productStates = $productStates.addClass( "selectedClass" );//nothing. earlier today, style=("fill-opacity","0.1") got tacked onto appropriate g tags
+//                                                $productStates.addClass("selectedClass").siblings("g").removeClass("selectedClass");//Rebecca Deprey suggested on 5/13/15
+
+        //                                    $productStates = $productStates.toggleClass( "selectedClass" );//does nothing
+        //                                    $productStates.toggleClass( "selectedClass" );//does nothing
+        //                                    $productStates = $productStates.attr( "class", "selectedClass" ).siblings( "g" ).removeAttr( "class","selectedClass" );//does nothing
+
+                                    }
+
+                                });
+                        });
+                    });
+
+                })
+
     });
 
 });
