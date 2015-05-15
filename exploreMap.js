37303f443e205a5e5ac09d3a95657222abf9506b
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2015-05-15 16:12:46 -0400

    exploreMap.js - .attr or .removeAttr do not take into account any other classes already applied to an element; just overwrites them. Rebecca’s function  checks for existing classes before adding and removing classes. Uses jQuery addClass and removeClass methods.

--- exploreMap.js
+++ exploreMap.js
@@ -7,7 +7,30 @@ $( document ).ready( function(){
         $( "g" ).on( "click", function (e) {
           console.log( "user clicked " + this.id );
 
-          $( this ).attr( "class", "clicked" ).siblings( "g" ).removeAttr( "class","clicked" ); //remove styling from un-clicked svg shapes
+          //gets the class attribute and checks for existing classes. Function courtesy of Rebecca.
+          $(this).attr("class", function(index, classNames) {
+              // if there are existing classes
+             if (typeof classNames != "undefined") {
+                 // return the existing classes and just add the clicked class to them
+                 return classNames + " clicked";
+                 // gets the class attribute and checks for existing classes on the "g" element
+                 $(this).siblings("g").attr("class", function(index, classNames) {
+                     // if there are existing classes
+                    if (typeof classNames != "undefined") {
+                        // return the existing classes and just remove the clicked class
+                        return classNames.replace("clicked", "");
+                    } else {
+                        // otherwise, remove the class attribute altogether
+                        $(this).siblings("g").removeAttr("class", "clicked");
+                    }
+                 });
+             } else {
+                 // if there aren't any existing classes, add the class clicked outright to the selected g element and remove the class from its siblings
+                $(this).attr("class", "clicked").siblings("g").removeAttr("class","clicked");
+             }
+          });
+
+          //$( this ).attr( "class", "clicked" ).siblings( "g" ).removeAttr( "class","clicked" ); //remove styling from un-clicked svg shapes
 
           selectedState = ( this.id );
           console.log( "so var selectedState is " + selectedState );
@@ -79,11 +102,14 @@ $( document ).ready( function(){
         });
 
 
-        $( "#productOptions" ).change( function( e ) {//.on can only bind to one function and .on is used on clicked g already. use .bind to bind ~?an el~? to multiple functions. Can .change do multiple events? it better or replace it.
+        $( "#productOptions" ).change( function( e ) {//.on can only bind to one function. .on is used for click event already. use .bind to bind ~?an el~? to multiple functions.
 
-                    $('.selectedClass').attr('class','');
+                    $('.selectedClass').attr('class','');// 5/13/15 Rebecca discovered that removing a class from SVGs works with empty quotes in arguments. But how to keep the selectedClass (chosen from drop down menu) on their appropriate SVG els despite that user clicked on any state, calling the clicked class.
 
-                    var selected = $("#productOptions option:selected").text();  //$spans.eq( 3 ).text( jQuery.inArray( "Pete", arr, 2 ) );
+
+
+
+                    var selected = $("#productOptions option:selected").text();
         //            console.log( "user selected " + selected );
         //            console.log( typeof selected );//string
 
@@ -102,18 +128,18 @@ $( document ).ready( function(){
 
                                 var productTypes = [];
                                 productTypes = ( obj.productTypes );
-        //                      console.log( productTypes );//an array of strings. How to iterate over each item and make object out of match? BTW, for Array objects, the toString method joins the array and returns one string containing each array element separated by commas.
+        //                      console.log( productTypes );//an array of strings. For Array objects, the toString method joins the array and returns one string containing each array element separated by commas.
         //                      console.log( typeof productTypes );//object
 
                                 if( Object.prototype.toString.call( productTypes ) === "[object Array]" ) {//test to make sure it is a true array, not array-like object. a prototype function.
         //                          console.log ( "Array" );
                                 }
 
-
-                                productTypes.every( function( entry ) { //every returns same as forEach from what I've tested so far. The every method executes the provided callback function once for each element present in the array until it finds one where callback returns a falsy value (a value that becomes false when converted to a Boolean). If such an element is found, the every method immediately returns false.
+        //                      Iterate over each item and make object out of match
+                                productTypes.every( function( entry ) { // .every returns same as .forEach from what I've tested so far. The every method executes the provided callback function once for each element present in the array until it finds one where callback returns a falsy value (a value that becomes false when converted to a Boolean). If such an element is found, the every method immediately returns false.
         //                          console.log( entry );//entry is the element(s) from array
         //                          console.log ( typeof entry );//string
-        //                          console.log ( selected );//my var, option the user selected
+        //                          console.log ( selected );//my var, option the user selected from drop down menu
         //                          console.log ( typeof selected );//string
 
                                     if (selected.indexOf( entry ) == -1 ) {//returns either the index/number of the start point for the string or a -1 meaning it isn’t there.
@@ -126,7 +152,7 @@ $( document ).ready( function(){
                                     var productKey = ( key );
                                         console.log ( "productKey " + productKey );//presence of a productKey means that corresponding state shapes (#g's) should get styled.
 
-                                    var $productStates =  $( "g" )
+                                    var $productStates =  $( "g" )//the $ in the var name indicates the var contains jQuery object(s) or is a jQuery collection
 
                                           .filter( function( index ) {
                                             if ( $( this ).attr( "id" ) == productKey ){//Within the filter function, this refers to each DOM element in turn.
@@ -135,15 +161,18 @@ $( document ).ready( function(){
                                             }
                                           })
                                             console.log ($productStates);
+        //                                    console.log ( typeof $productStates );//object; i.e., a jQuery collection of matched elements
+
+                                                // 5/13/15 In block below, I struggled to add and remove classes to the SVG g elements as the two classes are affecting each other. Also, SVG has finicky styling rules in which empty quotes may be needed to remove a class (see line 84).
+                                                // I have yet to figure out how to make this web app work so that when the user clicks on a state, the selectedClass doesn't go away; i.e., those white states remain white until the user chooses a new product type from the drop down menu.
 
         //                                        $productStates.css( "fill-opacity", "0.1" );//SUCCESS, but how to remove styling from unselected g's?
         //                                        $productStates = $productStates.attr("class","selectedClass");//class appears on tag but not actually styled with opacity change
         //                                        $productStates = $productStates.attr("class","selectedClass").siblings( "g" ).removeAttr( "class","selectedClass" );//class appears but quickly removes itself
                                                 $productStates.attr("class","selectedClass");//.siblings( "g" ).removeAttr( "class","selectedClass" );//class appears on tag but not actually styled with opacity change
                                                 //for line above, I indented one more tab in from where it was and now selectedClass is again appearing on tags but not actually styling the g's.
-//                                                $productStates = $productStates.addClass( "selectedClass" );//nothing. earlier today, style=("fill-opacity","0.1") got tacked onto appropriate g tags
-//                                                $productStates.addClass("selectedClass").siblings("g").removeClass("selectedClass");//Rebecca Deprey suggested on 5/13/15
-
+        //                                    $productStates = $productStates.addClass( "selectedClass" );//nothing. earlier today, style=("fill-opacity","0.1") got tacked onto appropriate g tags
+        //                                    $productStates.addClass("selectedClass").siblings("g").removeClass("selectedClass");//Rebecca suggested on 5/13/15
         //                                    $productStates = $productStates.toggleClass( "selectedClass" );//does nothing
         //                                    $productStates.toggleClass( "selectedClass" );//does nothing
         //                                    $productStates = $productStates.attr( "class", "selectedClass" ).siblings( "g" ).removeAttr( "class","selectedClass" );//does nothing
