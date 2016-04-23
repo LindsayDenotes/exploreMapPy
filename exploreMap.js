00c2b44e183bd5845e0bd2a00bf56fdc0ecc0e94
L Brown <lbrown@denotetoday.com>
2016-04-23 15:04:21 -0400

    Juan (#8)
    Juan,
    Thank you for sprucing up the code in exploreMap.js based on best practices (commenting and indentation tips, simplifying var declarations by getting rid of empty object or array, extracting functions so functions not coupled with DOM elements or other functions...). I agree about removing the beginnings of the Expert function. I did so when I started my refactor branch, which is specifically for refactoring exploreMap.js. It was good that you forked from master because, as of today (April 23, 2016), the refactor branch only contains the revealing module approach, despite that there's no need to privatize that data; however, if it were the case that it needed to be private and that pattern was practical to use, I did manage to pass the productTypes values outside of the module (i.e., function ParseJsonModule() ) by storing them in var i for each iteration through the contacts array.  I'll likely start a new branch based off your changes. That branch will be for modifying exploreMap.js in order to show just one state expert if the clicked state contains an expert for the productType selected in the dropdown.

--- exploreMap.js
+++ exploreMap.js
@@ -1,9 +1,9 @@
 /*
 Comments in all caps describe the event handler conditionals dealt with in that specific line or in the whole code block beneath the comment
 Comments to the right of a line pertain to that line only and are usually an interpretation in plain English of what that line does
-If I were to redo this code: 
+If I were to redo this code:
 1) add functionality to show upon click only the one contact at that state who matches the selected product type.
-2) perhaps name and wrap functions in immediately invoked function expressions (not hoisting bc fns would be in same lexical scope)  
+2) perhaps name and wrap functions in immediately invoked function expressions (not hoisting bc fns would be in same lexical scope)
 3) avoid pyramid of doom (select event handler has 4 levels of indentations; click has 5 levels. Eek! )
 
 Please visit denotetoday.com to see the responsive CSS version of this project
@@ -11,101 +11,68 @@ Please visit denotetoday.com to see the responsive CSS version of this project
 IDENTIFY FUNC INVOCATIONS, HIGHER ORDER FUNCS, AND THEIR CALLBACK FUNCS
 */
 
-$( document ).ready( function(){
-    var json = {};
-      $.getJSON( "stateInfoList.json", function( data ) {
-        //console.log ( data );//whole JSON object loads on document ready
+function toggleTextBox(index, classNames ){
+    if ( typeof classNames != "undefined" ) {//if any g has the clicked class instance on it...
+        $( "#txtDOT" ).show();//show the text box
+    } else {
+        console.log( "clicked doesn't exist so hide txtDOT" );
+        $( "#txtDOT" ).hide();//hide the text box; comes back when clicked class comes back
+    }
+}
 
-        //EVENT HANDLER FUNCTION (1 of 2): USER SELECTS FROM DROP DOWN MENU
-        $( "#productOptions" ).on( "change", function(e) {
+function dropDownHandler(data, e) {
+    $(this).css({
+        "border-style": "solid",
+        "border-color": "#333",
+        "border-width": "1px",
+        "outline": "1px dotted #333"
+    });
 
-        $( this ).css({ "border-style": "solid", "border-color": "#333", "border-width": "1px", "outline": "1px dotted #333"});
+    //Attributes methods. Selectors are classes.
+    $( ".clicked" ).attr( "class", "" );//DEFAULT BEHAVIOR: discard existing clicked class on both event handlers
+    $( ".selectedClass" ).attr( "class", "" );//discard the existing selected class.
 
-        //Attributes methods. Selectors are classes.
-            $( ".clicked" ).attr( "class", "" );//DEFAULT BEHAVIOR: discard existing clicked class on both event handlers
-            $( ".selectedClass" ).attr( "class", "" );//discard the existing selected class.
+    //Hide text box nested function
+    $( "g" ).attr( "class", toggleTextBox);
 
-        //Hide text box nested function
-            $( "g" ).attr( "class", function( index, classNames ){
-                if ( typeof classNames != "undefined" ) {//if any g has the clicked class instance on it...
-        //          console.log( "clicked exists so show txtDOT" );
-                    $( "#txtDOT" ).show();//show the text box
-                }
-                else {
-                    console.log( "clicked doesn't exist so hide txtDOT" );
-                    $( "#txtDOT" ).hide();//hide the text box; comes back when clicked class comes back
-                }
-            });
+    $.each (data, highlightStates);//closing for $.each ( data, function( key, val ){
+}
 
-        //Text object creation and filter functionality for the drop down menu
+function highlightStates(key, val){
+    var contacts = val.contacts;
+
+    contacts.forEach( function( obj ){
+
+        var productTypes = obj.productTypes;
+        productTypes.forEach( function( entry ) {
+
             var selected = $( "#productOptions option:selected" ).text();
-              console.log( "user selected " + selected );
-        //      console.log( typeof selected );//string
 
-            $.each ( data, function( key, val ){//parse JSON.
-        //        console.log ( key, val.contacts );//key is "nh" or "fl", val is whats inside json's { }s
+            if ( selected.indexOf( entry ) !== -1 ) {
+                var $productStates =  $( "g" ).filter(function(index) {
+                    if ( $( this ).attr( "id" ) == key ){
+                        return ( this.id );
+                    }
+                })
 
-        //      var jsonKey = ( key );
-        //          console.log ( "this is a state key: " + jsonKey );
+                $productStates.attr( "class", "selectedClass" );//add selectedClass attribute to the matched elements
+            }
+        });//closing for productTypes.forEach( function( entry ) {
 
-                var contacts = [];
-                contacts = ( val.contacts );
-        //          console.log ( "Referenced by val.contacts, contacts are " + contacts );//logs contacts are [object object]. So crack into those with a forEach loop.
+    });//closing for contacts.forEach( function( obj ){
 
-                    contacts.forEach( function( obj ){
+}
 
-                        var productTypes = [];
-                        productTypes = ( obj.productTypes );
-        //                    console.log( productTypes );//an array of strings. For Array objects, the toString method joins the array and returns one string containing each array element separated by commas.
-        //                    console.log( typeof productTypes );//object
+function handleStateInfo(data) {
 
-                        if( Object.prototype.toString.call( productTypes ) === "[object Array]" ) {//test to make sure it is a true array, not array-like object. a prototype function.
-        //                   console.log ( "Array" );
-                        }
+    //EVENT HANDLER FUNCTION (1 of 2): USER SELECTS FROM DROP DOWN MENU
+    $( "#productOptions" ).on( "change", function(ev) {
+        dropDownHandler(data, ev)
+    });
 
-                        //Iterate over each item in array and make object out of match
-                        productTypes.forEach( function( entry ) { //.forEach replaced the .every method - executed the provided callback function once for each element present in the array ONLY until it found one where callback returned a falsy value (a value that becomes false when converted to a Boolean). If such an element was found, the every method immediately returned false.
-        //                    console.log( entry );//entry is the element(s) from array
-        //                    console.log ( typeof entry );//string
-        //                    console.log ( selected );//my var, option the user selected from drop down menu
-        //                    console.log ( typeof selected );//string
-
-                            if ( selected.indexOf( entry ) == -1 ) {//if element doesn't exist...//returns either the index/number of the start point for the string or a -1 meaning it isn’t there.
-        //                      console.log( "element doesn't exist" );
-                            }
-                            else {//else, if element exists...
-        //                      console.log( "element found" );
-
-                            var productKey = ( key );
-        //                        console.log ( "productKey " + productKey );//presence of a productKey means that corresponding state shapes (#g's) should get styled via attr method.
-
-                            //Block below defines the selected states group; i.e., the states that use the product type that the user selected from the drop down menu.
-                            var $productStates =  $( "g" )//the $ in the var name indicates the var contains jQuery object(s) or is a jQuery collection.
-                                .filter( function( index ) {//within the filter function, this refers to each DOM element in turn.
-                                    if ( $( this ).attr( "id" ) == productKey ){//if the id of a g el matches a productKey
-        //                                console.log ( this.id );//GOOD. logged or, mn, etc., i.e., the states that have the product type which user selected in the drop down menu
-                                        return ( this.id );
-                                    }
-                                  })
-        //                      console.log ( $productStates );
-        //                      console.log ( typeof $productStates );//object; i.e., a jQuery collection of matched elements
-
-                            $productStates.attr( "class", "selectedClass" );//add selectedClass attribute to the matched elements
-
-                            }//closing for else {//else, if element exists...
-
-                        });//closing for productTypes.forEach( function( entry ) {
-
-                    });//closing for contacts.forEach( function( obj ){
-
-            });//closing for $.each ( data, function( key, val ){
-
-        });//closing for $( "#productOptions" ).on( "change", function (e){
-
-
-        //EVENT HANDLER FUNCTION (2 of 2): USER CLICKS ON A STATE SHAPE
-        $( "g" ).on( "click", function ( e ) {
-            $( "#txtDOT" ).show();
+    //EVENT HANDLER FUNCTION (2 of 2): USER CLICKS ON A STATE SHAPE
+    $( "g" ).on( "click", function ( e ) {
+        $( "#txtDOT" ).show();
 
         //Attributes function
         $( this ).attr( "class", function( index, classNames ) {
@@ -136,94 +103,54 @@ $( document ).ready( function(){
 
         //Data parsing and text object creation for text box below the map.
         clickedState = ( this.id );
-//          console.log( "so var clickedState is " + clickedState );
 
-        $.each( data, function( key, val ){
-//            console.log ( key, val );//key is "nh" or "fl", val is whats inside json's { }s
+        $.each( data, function( key, val ) {
+            parseStateData(clickedState, key, val)
+        });//closing for $.each( data, function( key, val ){
 
-            var jsonKey = ( key );
-                console.log ( "this is a state key: " + jsonKey );
+    });//closing for $( "g" )on( "click", function(e){
+}
 
-            if ( jsonKey == clickedState ){
-//                console.log( "clickedState ID " + clickedState + " MATCHES key: " + jsonKey + ". Return that val." );
+function parseStateData(clickedState, key, val) {
 
-                  var contacts = [];
-                  contacts = ( val.contacts );
-//                      console.log ( "Referenced by val.contacts, contacts are " + contacts );
+    if ( key == clickedState ){
 
+        var contacts = val.contacts;
 
-                  //text object creation
-                  var theText = "<dl class ='agency " + key + "'>" + val.agency + "</dl>";//<dl> tag defines a description list
+        //text object creation
+        var theText = "<dl class ='agency " + key + "'>" + val.agency + "</dl>";//<dl> tag defines a description list
 
-                    contacts.forEach( function( obj ){//The forEach() method executes a provided function once per array element.
+        contacts.forEach( function( obj ) {//The forEach() method executes a provided function once per array element.
 
-                        var productTypes = [];//is an array
-                        productTypes = ( obj.productTypes );//make the array into an object
-//                            console.log( productTypes );
-//                            console.log( typeof productTypes );//logged object
+            var productTypes = obj.productTypes;
 
+            var productTypesText = " ";//is string
+            productTypesText += productTypes;//the addition assignment operator adds the value of the right operand to a variable and assigns the result to the variable.
+            productTypesText = productTypesText.replace( /,/g , "<br/>" );//g stands for global, replace all matches, not just the first one. makes it a regular expression
 
-                        var productTypesText = " ";//is string
-                        productTypesText += productTypes;//the addition assignment operator adds the value of the right operand to a variable and assigns the result to the variable.
-                        productTypesText = productTypesText.replace( /,/g , "<br/>" );//g stands for global, replace all matches, not just the first one. makes it a regular expression
-//                            console.log( productTypesText );
-//                            console.log ( typeof productTypesText );//logged string
+            //obj for 5 non-participating states are empty strings.
+            var firstLast = obj.firstLast;
 
-                        // if productTypesText contains selected, return val.contact                        
-                        if ( $( "#productOptions option:selected" ) == productTypesText ) {
-                            
-                            function Expert(){
-                                
-                                var contact = {};
-                                contact = ( [obj] );
+            if ( firstLast !== " " ){//if firstLast obj is not an empty string, then...
+                var title = obj.title;
+                var phone = obj.phone;
+                var email = obj.email;
 
-                                function getExpert(){
-                                    console.log ( "referenced by [obj], contact is " + contact );
-                                }
-                                //return contacts index to make obj for a single contact
-                                return getExpert;
-                            }
-
-                            Expert = obj;
-
-                        }
-
-                        //obj for 5 non-participating states are empty strings.
-                        var firstLast = ( obj.firstLast );
-//                            console.log( firstLast );
-
-                        if ( firstLast !== " " ){//if firstLast obj is not an empty string, then...
-
-                        var title = ( obj.title );
-//                            console.log ( title );
-
-                        var phone = ( obj.phone );
-//                            console.log ( phone );
-
-                        var email = ( obj.email );
-//                            console.log ( email );
-
-                        //concatenate text objects
-                        theText += "<dt class='contacts'>" + firstLast + ", " + title + ", " + phone + ", " + email + "</dt>";//<dt> tag defines a term/name in the <dl> description list
-                        theText += "<dd class='productTypes'>" + productTypesText + "</dd>";//<dd> tag describes each <dt> term/name
-                        }
-
-                        else {//else, if firstLast obj is an empty string, then...
-
-                        var theMessage = ( obj.productTypes );//place a message where the productTypes obj would have been
-                            console.log( theMessage );
-                        theText += "<dt class='contacts'>" + theMessage + "</dt>";//yes, I want the message I wrote into ntpepInfo.xlsx's productTypes cell to be displayed in the contacts class.
-                        }
-
-                    });
-
-                  $( "#txtDOT" ).html( theText );
+                //concatenate text objects
+                theText += "<dt class='contacts'>" + firstLast + ", " + title + ", " + phone + ", " + email + "</dt>";//<dt> tag defines a term/name in the <dl> description list
+                theText += "<dd class='productTypes'>" + productTypesText + "</dd>";//<dd> tag describes each <dt> term/name
+            } else {//else, if firstLast obj is an empty string, then...
+                var theMessage = ( obj.productTypes );//place a message where the productTypes obj would have been
+                console.log( theMessage );
+                theText += "<dt class='contacts'>" + theMessage + "</dt>";//yes, I want the message I wrote into ntpepInfo.xlsx's productTypes cell to be displayed in the contacts class.
             }
 
-            });//closing for $.each( data, function( key, val ){
+        });
 
-        });//closing for $( "g" )on( "click", function(e){
+        $( "#txtDOT" ).html( theText );
+    }
+}
 
-      });//closing for $.getJSON( "stateInfoList.json", function( data ){
-
-});//closing for $(document).ready(function(){
+$( document ).ready( function(){
+    $.getJSON( "stateInfoList.json", handleStateInfo);
+})
